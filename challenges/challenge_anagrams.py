29e90e776d6ae23f7e5afdb13d1cc64f61408430
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-04-26 14:31:09 -0300

    feat req04

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,44 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    middle = len(arr) // 2
+    left = merge_sort(arr[:middle])
+    right = merge_sort(arr[middle:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+
+    return result
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+
+    if len(first_string) != len(second_string):
+        return (None, None, False)
+
+    sorted_first = merge_sort(list(first_string))
+    sorted_second = merge_sort(list(second_string))
+
+    return (
+        "".join(sorted_first),
+        "".join(sorted_second),
+        sorted_first == sorted_second,
+    )
