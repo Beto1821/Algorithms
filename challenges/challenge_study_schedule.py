b6dfc48449b9a98d2eeb10e6e2aa7a095cdb4897
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-04-25 22:06:28 -0300

    test req 1.1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,6 @@
 def study_schedule(permanence_period, target_time):
     count = 0
     for period in permanence_period:
-        if period[0] <= target_time and period[1] > target_time:
+        if period[0] <= target_time and period[1] >= target_time:
             count += 1
     return count
