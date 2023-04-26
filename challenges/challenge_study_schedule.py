4ef04427d1c953eec0d2f8bf180c3ba59c81671e
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-04-25 22:16:15 -0300

    req 1.1 testes

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,3 +4,21 @@ def study_schedule(permanence_period, target_time):
         if period[0] <= target_time and period[1] >= target_time:
             count += 1
     return count
+
+
+""" permanence_periods = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), ...], target_time = 5, expected = 3
+
+     @pytest.mark.parametrize(
+         "permanence_periods, target_time, expected",
+         [
+             ([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), (6, 7)], 5, 3),
+             ([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 1, 2),
+             ([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5)], 3, 2),
+             ([(1, 1), (2, 2), (3, 3), (4, 4)], 1, 1),
+             ([(1, 2), (1, 3), (2, 3)], 2, 3),
+         ],
+     )
+     def test_study_schedule_success(permanence_periods, target_time, expected):
+ >       assert study_schedule(permanence_periods, target_time) == expected
+ E       assert 0 == 3
+ E        +  where 0 = study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), ...], 5) """
