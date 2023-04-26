4ebd14a6a63a0c5315fd53645d68f1b69387bc17
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-04-25 23:19:21 -0300

    feat req02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,24 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    # Teste com key maior que o tamanho da mensagem
+    assert encrypt_message("adalberto", 33) == "otreblada"
+
+    # Teste com key ímpar
+    assert encrypt_message("adalberto", 3) == "ada_otrebl"
+
+    # Teste com key par
+    assert encrypt_message("adalberto", 4) == "otreb_lada"
+
+    # Teste com a Key negativa
+    assert encrypt_message("adalberto", -1) == "otreblada"
+
+    # Teste com tipo inválido para message
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message([], 13)
+
+    # Teste com tipo inválido para key
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("", "")
